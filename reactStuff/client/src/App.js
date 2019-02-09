6d20012dc020070085dda11915cca9e4449349b6
Tommmy Collison <tommy@collison.ie>
2019-02-09 15:14:36 -0800

    login on progress

--- reactStuff/client/src/App.js
+++ reactStuff/client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import {Route, NavLink} from 'react-router-dom';
 import jokes from './jokes/jokes';
+import login from './jokes/login';
 
 class App extends Component {
   render() {
@@ -10,10 +11,12 @@ class App extends Component {
       <div className='navLinks'>
         <NavLink exact to='/'>Home</NavLink>
         <NavLink exact to='/jokes'>Jokes</NavLink>
+        <NavLink exact to='/login'>Log in</NavLink>
       </div>
       <h1>The Dad Joke Website</h1>
-      <p>You must be authenticated to view the jokes.</p>
+      <p>Like elks on the serenghetti, dads must, from time to time, return to the canonical source of all dad jokes to replenish their stock.</p>
       <Route path='/jokes' component={jokes} />
+      <Route path='/login' component={login} />
       </div>
     );
   }
