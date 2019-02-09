b594b0b1d4b80239820ae16456e38f4d1ce6a530
Tommmy Collison <tommy@collison.ie>
2019-02-09 09:49:20 -0800

    fixes server error

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const session = require('express-session')
 
 const configureRoutes = require('../config/routes.js');
 
