87436de2295580be3c00f268d0a566f61f9f816c
Tommmy Collison <tommy@collison.ie>
2019-02-09 14:27:06 -0800

    adds navLinks

--- reactStuff/client/src/App.js
+++ reactStuff/client/src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import {Route, NavLink} from 'react-router-dom';
+import jokes from './jokes/jokes';
 
 class App extends Component {
   render() {
     return (
-      <div className="Container">
+      <div className='Container'>
+      <div className='navLinks'>
+        <NavLink exact to='/'>Home</NavLink>
+        <NavLink exact to='/jokes'>Jokes</NavLink>
+      </div>
       <h1>The Dad Joke Website</h1>
       <p>You must be authenticated to view the jokes.</p>
+      <Route path='/jokes' component={jokes} />
       </div>
     );
   }
