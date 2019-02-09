3a72e5808d3a500ea0a8ffbe5825f8f4a279f6dc
Tommmy Collison <tommy@collison.ie>
2019-02-09 12:54:24 -0800

    comments out the getJokes function in routes so I can use it in index

--- config/routes.js
+++ config/routes.js
@@ -42,16 +42,16 @@ function login(req, res) {
 // }
 
 function getJokes(req, res) {
-  const requestOptions = {
-    headers: { accept: 'application/json' },
-  };
+  // const requestOptions = {
+  //   headers: { accept: 'application/json' },
+  // };
 
-  axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
-    .then(response => {
-      res.status(200).json(response.data.results);
-    })
-    .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
-    });
+  // axios
+  //   .get('https://icanhazdadjoke.com/search', requestOptions)
+  //   .then(response => {
+  //     res.status(200).json(response.data.results);
+  //   })
+  //   .catch(err => {
+  //     res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+  //   });
 }
